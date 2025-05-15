3c649a47b11a3c6706250eab564a836d17ac0822
Vlad <laydown322@gmail.com>
2025-05-15 15:17:03 +0300

    Merge branch 'main' into Game-Design

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+    public void StartGame() {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void ExitGame() {
+        Application.Quit(GetHashCode());
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Crop data
+    [SerializeField] private GameObject[] seedPrefabs;
+    [SerializeField] private float interactRange = 1.5f;
+    [SerializeField] private LayerMask soilLayer;
+    [SerializeField] private LayerMask plantLayer;
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public int CurrentHealth => currentHealth;
+    public int MaxHealth => maxHealth;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    private void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (!TryHarvestPlantNearby()) {
+                TryPlantSeedNearby();
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        rb.linearVelocity = movement.normalized * moveSpeed;
+    }
+
+    // Handles the player's different interactions
+    private void TryPlantSeedNearby() {
+        Collider2D[] nearbySoils = Physics2D.OverlapCircleAll(transform.position, interactRange, soilLayer);
+
+        foreach (Collider2D soilCollider in nearbySoils) {
+            SoilTile soil = soilCollider.GetComponent<SoilTile>();
+            if (soil != null && !soil.IsOccupied) {
+                int selectedSeedIndex = Random.Range(0, seedPrefabs.Length);
+                soil.PlantSeed(seedPrefabs[selectedSeedIndex]);
+                return;
+            }
+        }
+    }
+
+    private bool TryHarvestPlantNearby() {
+        Collider2D[] nearbyPlants = Physics2D.OverlapCircleAll(transform.position, interactRange, plantLayer);
+
+        foreach (Collider2D col in nearbyPlants) {
+            Plant plant = col.GetComponent<Plant>();
+            if (plant != null && plant.IsFullyGrown) {
+                SoilTile soil = col.GetComponentInParent<SoilTile>();
+                plant.Harvest();
+                if (soil != null) {
+                    soil.ClearSoil();
+                }
+                return true; // Harvested one plant, exit
+            }
+        }
+        return false; // No harvestable plant found
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, interactRange);
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Destroy(this.gameObject);
+    }
+}
